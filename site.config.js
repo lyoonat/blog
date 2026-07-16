f7f0dd397161bec0669b1378801e7d488747abfc
SeungJi Natalie Lyoo <lyoonatelie@gmail.com>
2026-07-17 00:29:41 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "Natly",
-    image: "/tibbixel-dot-com-1542-svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/tibbixel-dot-com-1542-svg.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "Backend dev-yet",
     bio: "Living Contradiction",
     email: "lyoonatelie@gmail.com",
