98466acf7d5c5fb67dd4a66c5c176b8f153c7fc9
SeungJi Natalie Lyoo <lyoonatelie@gmail.com>
2026-07-20 14:27:02 +0900

    Update Logo.tsx

--- src/layouts/RootLayout/Header/Logo.tsx
+++ src/layouts/RootLayout/Header/Logo.tsx
@@ -4,12 +4,21 @@ import styled from "@emotion/styled"
 
 const Logo = () => {
   return (
-    <StyledWrapper href="/" aria-label={CONFIG.blog.title}>
-      {CONFIG.blog.title}
+    <StyledWrapper href="/" aria-label={CONFIG.profile.name}>
+      <img
+        src={CONFIG.profile.image}
+        alt={CONFIG.profile.name}
+        width={40}
+        height={40}
+        style={{ borderRadius: "50%" }}
+      />
     </StyledWrapper>
   )
 }
 
 export default Logo
 
-const StyledWrapper = styled(Link)``
+const StyledWrapper = styled(Link)`
+  display: flex;
+  align-items: center;
+`
