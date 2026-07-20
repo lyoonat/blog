53df4e03d1b6a8a9e238bb9a060fbd0dd4584608
SeungJi Natalie Lyoo <lyoonatelie@gmail.com>
2026-07-20 19:42:42 +0900

    Revise icon

--- src/layouts/RootLayout/Header/ThemeToggle.tsx
+++ src/layouts/RootLayout/Header/ThemeToggle.tsx
@@ -14,7 +14,7 @@ const ThemeToggle: React.FC<Props> = () => {
 
   return (
     <StyledWrapper onClick={handleClick}>
-      <Emoji>{scheme === "light" ? "☀️" : "🌙"}</Emoji>
+      <Emoji>{scheme === "light" ? "☀️" : "\u{1FA7B}"}</Emoji>
     </StyledWrapper>
   )
 }
--- src/routes/Feed/ServiceCard.tsx
+++ src/routes/Feed/ServiceCard.tsx
@@ -9,7 +9,7 @@ const ServiceCard: React.FC = () => {
   return (
     <>
       <StyledTitle>
-        <Emoji>🌟</Emoji> Service
+        <Emoji>"\<u>1F50B</u>"</Emoji> Service
       </StyledTitle>
       <StyledWrapper>
         {CONFIG.projects.map((project, idx) => (
